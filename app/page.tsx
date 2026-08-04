ce79398e993bec061322f305362e829e39878d20
worldhuang <worldhuang2002@163.com>
2026-08-04 10:58:22 +0800

    Remove login and add standalone webpage

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,5 @@
-import { cookies } from "next/headers";
-import { COOKIE_NAME, createAccessToken } from "./access-token";
-import PasswordGate from "./PasswordGate";
 import Planner from "./Planner";
 
-export const dynamic = "force-dynamic";
-
-export default async function Home() {
-  const cookieStore = await cookies();
-  const suppliedToken = cookieStore.get(COOKIE_NAME)?.value;
-  const expectedToken = await createAccessToken();
-
-  if (!expectedToken || suppliedToken !== expectedToken) {
-    return <PasswordGate />;
-  }
-
+export default function Home() {
   return <Planner />;
 }
